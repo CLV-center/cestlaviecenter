81ad2f23ecaaf321c6874b64374ebc71062508aa
CLV-center <112400677+CLV-center@users.noreply.github.com>
2022-09-06 12:47:49 +0200

    first commit

---
+++ javaScript/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type",{
+    strings: ["French", "English", "Music", "History", "Art", "Arabic", "Quran", "Science"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
